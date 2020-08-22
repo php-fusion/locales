bc243bf5f3f22d9611dc7b1ca7f702df1e263f06
RobiNN <kelcakrobo@gmail.com>
2020-08-22 21:21:32 +0200

    Update Slovak

--- Slovak/locale/Slovak/admin/members_email.php
+++ Slovak/locale/Slovak/admin/members_email.php
@@ -9,11 +9,7 @@ Teraz sa môžete prihlásiť pomocou zvoleného mena a hesla.<br/>\r\nS pozdrav
 $locale['email_deactivate_subject'] = "Vyžaduje sa reaktivácia účtu na ";
 $locale['email_deactivate_message'] = "Ahoj [USER_NAME],<br/>\r\nUplynulo [DEACTIVATION_PERIOD] dní od vášho posledného prihlásenia na [SITENAME]. Boli ste označený ako neaktívny, ale údaje vášho účtu ostávajú nezmenené.<br/>\r\n
 Na reaktiváciu vášho účtu kliknite na tento odkaz: [REACTIVATION_LINK]<br/>\r\nS pozdravom,<br/>\r\n[SITEUSERNAME]";
-$locale['email_deactivate_message'] = "Ahoj [USER_NAME],<br/>\r\nUplynulo [DEACTIVATION_PERIOD] dní od vášho posledného prihlásenia na [SITENAME]. Boli ste označený ako neaktívny, ale údaje vášho účtu ostávajú nezmenené.<br/>\r\n
-Na reaktiváciu vášho účtu kliknite na tento odkaz: [REACTIVATION_LINK]<br/>\r\nS pozdravom,<br/>\r\n[SITEUSERNAME]";
 $locale['email_ban_subject'] = "Váš účet na [SITENAME] bol zabanovaný";
-$locale['email_ban_message'] = "Ahoj [USER_NAME],<br/>\r\nVáš účet na [SITENAME] bol zabanovaný od [ADMIN_USERNAME] z tohto dôvodu:<br/>\r\n
-[REASON]<br/>\r\nAk chcete získať viac informácií o tomto zákaze, kontaktujte správcu webu na [SITENAME].<br/>\r\nS pozdravom,<br/>\r\n[SITEUSERNAME]";
 $locale['email_ban_message'] = "Ahoj [USER_NAME],<br/>\r\nVáš účet na [SITENAME] bol zabanovaný od [ADMIN_USERNAME] z tohto dôvodu:<br/>\r\n
 [REASON]<br/>\r\nAk chcete získať viac informácií o tomto zákaze, kontaktujte správcu webu na [SITENAME].<br/>\r\nS pozdravom,<br/>\r\n[SITEUSERNAME]";
 $locale['email_secban_subject'] = "Váš účet na [SITENAME] bol zabanovaný";
