578c18ca177b8dcdbfdb82c8ee4bd0010b3b5a48
RobiNN <kelcakrobo@gmail.com>
2020-12-11 00:28:19 +0100

    Update Turkish

--- Turkish/locale/Turkish/global.php
+++ Turkish/locale/Turkish/global.php
@@ -1,18 +1,12 @@
 <?php
 $locale['setlocale'] = "tr_TR"; // https://www.php.net/manual/en/function.setlocale.php
-$locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
-$locale['region'] = "TR"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
-$locale['xml_lang'] = "tr";
-$locale['short_lang_name'] = "tr";
+$locale['charset'] = "utf-8"; // https://www.iana.org/assignments/character-sets/character-sets.xhtml
+$locale['region'] = "TR"; // ISO-3166 (Alpha-2 code) https://www.iso.org/obp/ui/#search
+$locale['xml_lang'] = "tr"; // ISO 639-1 Code http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['tinymce'] = "tr"; // refer to path includes/jscripts/tinymce/langs/{value}.js
-$locale['select2'] = "tr"; // refer to path includes/jscripts/select2/select2_locale_{value}.js
+$locale['select2'] = "tr"; // refer to path includes/dynamics/assets/select2/select2_locale_{value}.js
 $locale['phpmailer'] = "tr"; // refer to path includes/classes/PHPMailer/language/phpmailer.lang-{value}.php
 $locale['filemanager'] = "tr"; // refer to path includes/elFinder/js/i18n/{value}.js
-$locale['datepicker'] = "tr";
-$locale['datepicker_js'] = "A-GG-YYYY S:dd:ss";
-$locale['datepicker_php'] = "a-g-Y S:d:s";
-$locale['password_strength'] = "tr";
-$locale['date_day'] = "%d %b";
 $locale['text-direction'] = "ltr";
 
 // Full & Short Months
